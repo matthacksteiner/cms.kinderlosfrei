72135be30c26116bc878b13921a6be92fc769aaa
Matthias Hacksteiner <contact@matthiashacksteiner.net>
2023-12-21 21:00:49 +0100

    Add srcset sizes for image thumbnails

--- site/plugins/baukasten-srcset/index.php
+++ site/plugins/baukasten-srcset/index.php
@@ -4,8 +4,21 @@ use Kirby\Cms\App as Kirby;
 
 Kirby::plugin('baukasten/srcset', [
     'hooks' => [
-        'your-plugin.your-hook' => function ($page) {
-            // do whatever you need
+        'file.create:after' => function ($file) {
+            // Check if the file is an image
+            if ($file->type() === 'image') {
+                // Get different sizes for srcset
+                $thumbSmall = $file->thumb(['width' => 320]);
+                $thumbMedium = $file->thumb(['width' => 640]);
+                $thumbLarge = $file->thumb(['width' => 1024]);
+
+                // Store the URLs of the generated images in the file's metadata
+                $file->update([
+                    'thumbSmall' => $thumbSmall->url(),
+                    'thumbMedium' => $thumbMedium->url(),
+                    'thumbLarge' => $thumbLarge->url(),
+                ]);
+            }
         },
     ]
 ]);
