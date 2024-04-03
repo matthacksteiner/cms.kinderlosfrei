fc28f1eb323c241ce3473d3ae4c48bd68ed4fd0e
Matthias Hacksteiner <contact@matthiashacksteiner.net>
2024-04-03 13:42:43 +0200

    added fonts for section

--- site/templates/section.json.php
+++ site/templates/section.json.php
@@ -47,56 +47,74 @@ function getSettings(\Kirby\Cms\Page $page)
     'ratioMobile' => $page->ratioMobile()->value(),
     'spanMobile' => $page->spanMobile()->value(),
     'span' => $page->span()->value(),
-    'title' => $page->titleSettings()->textfont()->value(),
+    'main' => [
+      'level' => $page->mainLevel()->value(),
+      'font' => $page->mainFont()->value(),
+      'size' => $page->mainSize()->value(),
+      'color' => $page->mainColor()->value(),
+      'align' => $page->mainAlign()->value(),
+    ],
+    'title' => [
+      'level' => $page->level()->value(),
+      'font' => $page->titleFont()->value(),
+      'size' => $page->titleSize()->value(),
+      'color' => $page->titleColor()->value(),
+      'align' => $page->titleAlign()->value(),
+    ],
+    'text' => [
+      'font' => $page->textFont()->value(),
+      'size' => $page->textSize()->value(),
+      'color' => $page->textColor()->value(),
+      'align' => $page->textAlign()->value(),
+    ]
   ];
 }
 
 $json['settings'] = getSettings($page);
 
+if (method_exists($page, 'getJsonData')) {
+  $content = $page->content()->toArray();
+  $unsetFields = [
+    'title',
+    'meta_title',
+    'meta_description',
+    'meta_canonical_url',
+    'meta_author',
+    'meta_image',
+    'meta_phone_number',
+    'og_title',
+    'og_description',
+    'og_image',
+    'og_site_name',
+    'og_url',
+    'og_audio',
+    'og_video',
+    'og_determiner',
+    'og_type',
+    'og_type_article_published_time',
+    'og_type_article_modified_time',
+    'og_type_article_expiration_time',
+    'og_type_article_author',
+    'og_type_article_section',
+    'og_type_article_tag',
+    'twitter_title',
+    'twitter_description',
+    'twitter_image',
+    'twitter_card_type',
+    'twitter_site',
+    'twitter_creator',
+    'robots_noindex',
+    'robots_nofollow',
+    'robots_noarchive',
+    'robots_noimageindex',
+    'robots_nosnippet',
+    'blocks'
+  ];
 
+  foreach ($unsetFields as $key) {
+    unset($content[$key]);
+  }
+}
 
-// if (method_exists($page, 'getJsonData')) {
-//   $content = $page->content()->toArray();
-//   $unsetFields = [
-//     'title',
-//     'meta_title',
-//     'meta_description',
-//     'meta_canonical_url',
-//     'meta_author',
-//     'meta_image',
-//     'meta_phone_number',
-//     'og_title',
-//     'og_description',
-//     'og_image',
-//     'og_site_name',
-//     'og_url',
-//     'og_audio',
-//     'og_video',
-//     'og_determiner',
-//     'og_type',
-//     'og_type_article_published_time',
-//     'og_type_article_modified_time',
-//     'og_type_article_expiration_time',
-//     'og_type_article_author',
-//     'og_type_article_section',
-//     'og_type_article_tag',
-//     'twitter_title',
-//     'twitter_description',
-//     'twitter_image',
-//     'twitter_card_type',
-//     'twitter_site',
-//     'twitter_creator',
-//     'robots_noindex',
-//     'robots_nofollow',
-//     'robots_noarchive',
-//     'robots_noimageindex',
-//     'robots_nosnippet',
-//     'blocks',
-//   ];
-
-//   foreach ($unsetFields as $key) {
-//     unset($content[$key]);
-//   }
-// }
 
 echo json_encode($json);
