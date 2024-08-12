7d2f717ddd145ac2b45095047fdc5a61343d6916
Matthias Hacksteiner <contact@matthiashacksteiner.net>
2024-08-12 15:19:54 +0200

    forgot to paste rest of code

--- site/plugins/baukasten-blocks/index.php
+++ site/plugins/baukasten-blocks/index.php
@@ -136,7 +136,149 @@ function getBlockArray(\Kirby\Cms\Block $block)
             $blockArray['content']['image'] = $image;
             break;
 
-            // Additional cases follow similarly...
+        case "vector":
+            $blockArray['content'] = $block->toArray()['content'];
+            $image = null;
+            if ($file1 = $block->image()->toFile()) {
+                $image = [
+                    'url' => $file1->url(),
+                    'alt' => (string)$file1->alt(),
+                    'identifier' => $file1->identifier()->value(),
+                    'classes' => $file1->classes()->value(),
+                    'width' => $file1->width(),
+                    'height' => $file1->height(),
+                    'captiontoggle' => $file1->captiontoggle()->toBool(false),
+                    'captiontitle' => $file1->captionobject()->toObject()->captiontitle()->value(),
+                    'captiontextfont' => $file1->captionobject()->toObject()->textfont()->value(),
+                    'captiontextsize' => $file1->captionobject()->toObject()->textsize()->value(),
+                    'captiontextcolor' => $file1->captionobject()->toObject()->textColor()->value(),
+                    'captiontextalign' => $file1->captionobject()->toObject()->textalign()->value(),
+                    'captionoverlay' => $file1->captionobject()->toObject()->captionControls()->options()->value(),
+                    'captionalign' => $file1->captionobject()->toObject()->captionalign()->value(),
+                    'linktoggle' => $file1->linktoggle()->toBool(false),
+                    'linkexternal' => getLinkArray($file1->linkexternal()),
+                ];
+            }
+
+            $blockArray['content']['image'] = $image;
+            break;
+
+        case 'slider':
+            $blockArray['content'] = $block->toArray()['content'];
+            $images = [];
+
+            foreach ($block->images()->toFiles() as $file) {
+                $image = $file;
+
+                $ratioMobile = explode('/', $block->ratioMobile()->value());
+                $ratio = explode('/', $block->ratio()->value());
+
+                $calculateHeight = function ($width, $ratio) {
+                    return isset($ratio[1]) ? round(($width / $ratio[0]) * $ratio[1]) : $width;
+                };
+
+                $images[] = [
+                    'url' => $image->url(),
+                    'urlFocus' => $image->crop($image->width(), $calculateHeight($image->width(), $ratio))->url(),
+                    'urlFocusMobile' => $image->crop($image->width(), $calculateHeight($image->width(), $ratioMobile))->url(),
+                    'width' => $image->width(),
+                    'height' => $image->height(),
+                    'alt' => (string)$image->alt(),
+                    'identifier' => $image->identifier()->value(),
+                    'classes' => $image->classes()->value(),
+                    'focusX' => json_decode($file->focusPercentageX()),
+                    'focusY' => json_decode($file->focusPercentageY()),
+                    'toggle' => $file->toggle()->toBool(false),
+                    'captiontoggle' => $file->captiontoggle()->toBool(false),
+                    'captiontitle' => $file->captionobject()->toObject()->captiontitle()->value(),
+                    'captiontextfont' => $file->captionobject()->toObject()->textfont()->value(),
+                    'captiontextsize' => $file->captionobject()->toObject()->textsize()->value(),
+                    'captiontextcolor' => $file->captionobject()->toObject()->textColor()->value(),
+                    'captiontextalign' => $file->captionobject()->toObject()->textalign()->value(),
+                    'captionoverlay' => $file->captionobject()->toObject()->captionoverlay()->value(),
+                    'captionalign' => $file->captionobject()->toObject()->captionalign()->value(),
+                    'linktoggle' => $file->linktoggle()->toBool(false),
+                    'linkexternal' => getLinkArray($file->linkexternal()),
+                ];
+            }
+
+            $blockArray['content']['images'] = $images;
+            $blockArray['content']['toggle'] = $block->toggle()->toBool(false);
+
+            break;
+
+        case "menu":
+            $blockArray['content'] = $block->toArray()['content'];
+            foreach ($block->nav()->toStructure() as $key => $item) {
+                $linkobject = [];
+                if ($item->linkobject()->isNotEmpty()) {
+                    $linkobject = getLinkArray($item->linkobject());
+                }
+                $blockArray['content']['nav'][$key]["linkobject"] = $linkobject;
+            }
+
+            break;
+
+        case 'button':
+            $blockArray['content'] = $block->toArray()['content'];
+
+            $linkobject = [];
+            if ($block->linkobject()->isNotEmpty()) {
+                $linkobject = getLinkArray($block->linkobject());
+                $blockArray['content']['linkobject'] = $linkobject;
+            }
+
+            break;
+
+        case 'text':
+            $blockArray['content'] = $block->toArray()['content'];
+            $blockArray['content']['text'] = (string)$block->text();
+            break;
+
+        case "iconlist":
+            $blockArray['content'] = $block->toArray()['content'];
+
+            foreach ($block->list()->toStructure() as $key => $item) {
+                $icon = null;
+                if ($file = $item->icon()->toFile()) {
+                    $icon = [
+                        'url' => $file->url(),
+                        'alt' => (string)$file->alt(),
+                        'source' => file_get_contents($file->root()),
+                    ];
+                }
+
+                $blockArray['content']['list'][$key]["icon"] = $icon;
+            }
+
+            break;
+
+        case 'code':
+            $blockArray['content'] = $block->toArray()['content'];
+            $blockArray['content']['code'] = (string)$block->code();
+            break;
+
+        case 'video':
+            $blockArray['content'] = $block->toArray()['content'];
+            $video = null;
+            $thumb = null;
+            if ($file1 = $block->file()->toFile()) {
+                $video = [
+                    'url' => $file1->url(),
+                    'alt' => (string)$file1->alt(),
+                    'identifier' => $file1->identifier()->value(),
+                    'classes' => $file1->classes()->value(),
+                ];
+            }
+            if ($file2 = $block->thumbnail()->toFile()) {
+                $thumb = [
+                    'url' => $file2->url(),
+                    'alt' => (string)$file2->alt(),
+                ];
+            }
+            $blockArray['content']['thumbnail'] = $thumb;
+            $blockArray['content']['file'] = $video;
+            break;
 
         default:
             $blockArray['content'] = $block->toArray()['content'];
