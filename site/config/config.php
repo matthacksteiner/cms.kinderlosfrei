1f8f0d9fdef1176f1b5091436ed9ff98b3bb8f03
Matthias Hacksteiner <contact@matthiashacksteiner.net>
2025-06-02 10:44:51 +0200

    .env file for deploy

--- site/config/config.php
+++ site/config/config.php
@@ -4,6 +4,10 @@ use Kirby\Filesystem\Dir;
 use Kirby\Filesystem\F;
 use Kirby\Http\Response;
 
+// Load environment variables before configuration
+require_once __DIR__ . '/../plugins/kirby3-dotenv/global.php';
+loadenv();
+
 /*
 |--------------------------------------------------------------------------
 | Kirby Configuration Array
@@ -22,7 +26,7 @@ return [
 	'prefixDefaultLocale' => false,
 	'error'           => 'z-error',
 	'johannschopplich.deploy-trigger' => [
-		'deployUrl' => 'https://api.netlify.com/build_hooks/65142ee2a2de9b24080dcc95',
+		'deployUrl' => env('DEPLOY_URL', 'https://api.netlify.com/build_hooks/65142ee2a2de9b24080dcc95'),
 	],
 	'panel' => [
 		'css'     => 'assets/css/baukasten-panel.css',
